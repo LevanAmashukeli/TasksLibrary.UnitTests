91edda70743b3fa7e68acd2b76e13d66bdd930ac
nuciko24 <nuciko24@list.ru>
2022-06-09 17:35:22 +0400

    new tests String Assert

---
+++ TasksLibrary.Tests/StringAssertMethods.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
+
+namespace TasksLibrary.Tests
+{
+    [TestClass]
+    public class StringAssertMethods
+    {
+        [TestMethod]
+        public void StringContainsTest()
+        {
+            // check for occurrence of a substring in a string
+            StringAssert.Contains("Assert samples", "sam");
+        }
+
+        [TestMethod]
+        public void StringMatchTest()
+        {
+            // check with use RegEx
+            StringAssert.Matches("123", new Regex(@"\d{3}"));
+        }
+
+        [TestMethod]
+        public void StringStartsWithTest()
+        {
+            // check , if string starts with concrette word
+            StringAssert.StartsWith("Hello world", "Hello");
+        }
+
+        [TestMethod]
+        public void StringEndsWithTest()
+        {
+            // check , if string ends with concrette word
+            StringAssert.StartsWith("Hello world", "world");
+        }
+    }
+}
